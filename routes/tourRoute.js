43085e2b1d80709342d32433f9fb8eb05589ca24
Illya1123 <21520565@gm.uit.edu.vn>
2024-12-19 15:35:17 +0700

    hoàn thiện api hỗ trợ tạo tour

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -5,6 +5,7 @@ const router = require('express').Router();
 
 router.route('/create-tour').post(authentication, restrictTo('0'), createTour);
 router.route('/get-all-tour/').get(authentication, restrictTo('0'), getAllTour);
+router.route('/getAllTour').get(getAllTour);
 router.route('/get-tour/:id').get(getTourById);
 
 module.exports = router;
